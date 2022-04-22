ca0415e8fcb891dc71d6b9ddbe054409f6470b2e
RIN <rin.saito61@gmail.com>
2022-04-22 23:45:56 +0900

    test.pyの変更

--- test2.py
+++ test2.py
@@ -1,13 +1,12 @@
-'''初めてのkivy'''
 from kivy.app import App
 from kivy.uix.label import Label
-
 from kivy.core.text import LabelBase, DEFAULT_FONT
 from kivy.resources import resource_add_path
 
 resource_add_path('C:\Windows\Fonts')
-LabelBase.register(DEFAULT_FONT,'HGRPP1.TTC')
-class TestApp(App):
+LabelBase.register(DEFAULT_FONT,'HGRPRE.TTC')
+
+class Test2App(App):
     pass
 
-TestApp().run()
+Test2App().run()
